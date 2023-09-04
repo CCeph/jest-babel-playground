a7bb94f82734ee2fd55c2b663f7f179821dd091e
CCeph <yousefelsisyy@gmail.com>
2023-09-04 11:13:24 -0300

    Add second capitalize test

--- utils.test.js
+++ utils.test.js
@@ -3,3 +3,7 @@ import capitalize from "./utils.js";
 test('First letter is capital from capitalize', () => {
     expect(capitalize('test')[0]).toMatch(/[A-Z]/);
 });
+
+test("Remaining letters are lowercase", () => {
+    expect(capitalize('test').substring(1,)).not.toMatch(/[A-Z]/);
+})
