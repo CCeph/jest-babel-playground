4ce3c16a8759aa715fea90d66cd16287ddde8314
CCeph <yousefelsisyy@gmail.com>
2023-09-04 11:09:34 -0300

    Add capitalize function that passes tests

---
+++ utils.js
@@ -0,0 +1,5 @@
+export default function capitalize(string) {
+    let firstLetter = string[0].toUpperCase();
+    let remainingLetters = string.substring(1,);
+    return firstLetter + remainingLetters;
+}
--- utils.test.js
+++ utils.test.js
@@ -1,3 +1,5 @@
+import capitalize from "./utils.js";
+
 test('First letter is capital from capitalize', () => {
     expect(capitalize('test')[0]).toMatch(/[A-Z]/);
 });
