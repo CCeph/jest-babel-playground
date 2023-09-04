95b384d9d5478f461c10b8fb7bd6b99277b58c55
CCeph <yousefelsisyy@gmail.com>
2023-09-04 11:20:56 -0300

    Add tests for reverseString

--- utils.test.js
+++ utils.test.js
@@ -7,3 +7,11 @@ test('First letter is capital from capitalize', () => {
 test("Remaining letters are lowercase", () => {
     expect(capitalize('test').substring(1,)).not.toMatch(/[A-Z]/);
 })
+
+test('reverseString: String with numbers is reversed', () => {
+    expect(reverseString('12345')).toBe('54321');
+})
+
+test('reverseString: String with letters and numbers is reversed', () => {
+    expect(reverseString('ell0529World').toBe('dlroW9250lle'));
+})
